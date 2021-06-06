702825e0f1ebf91955f239166a3738e5dd4bdef0
Guilherme Coimbra <guilherme.xavier27@outlook.com>
2021-06-05 23:27:02 -0300

    aula 3 terminada

---
+++ DevFitness.API/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using DevFitness.API.Core.Entities;
+using DevFitness.API.Models.InputModels;
+using DevFitness.API.Models.ViewModels;
+using DevFitness.API.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    //api/users
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DevFitnessDbContext _dbContext;
+        public UsersController(DevFitnessDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var users = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+            if (users == null)
+                return NotFound();
+
+            var userViewModel = new UserViewModel(users.Id, users.FullName, users.Height, users.Weight, users.BirthDate);
+            return Ok(userViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel inputModel)
+        {
+            var user = new User(inputModel.FullName, inputModel.Height, inputModel.Weight, inputModel.BirthDate);
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, inputModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateUserInputModel inputModel)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+            user.Update(inputModel.Height, inputModel.Weight);
+
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
